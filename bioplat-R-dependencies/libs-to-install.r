93d479775c53f951065a160c74d2c6ff03839286
diego <diegoariel83@gmail.com>
2016-07-23 17:03:53 -0300

    imagen con las dependencias para bioplat R. ver libs-to-install.r para tener un detalle de cuáles son actualmente

---
+++ bioplat-R-dependencies/libs-to-install.r
@@ -0,0 +1,34 @@
+
+#libs propias
+#install.packages('/r/bioplatR-1.0.tar.gz', repos=NULL, type="source")
+
+
+#libs de terceros
+affy=source("http://bioconductor.org/biocLite.R");biocLite("affy")
+
+frma=source("http://bioconductor.org/biocLite.R");biocLite("frma")
+
+survival=install.packages("survival")
+
+fpc=install.packages("fpc")
+
+inSilicoDb=source("http://www.bioconductor.org/biocLite.R");biocLite("inSilicoDb")
+
+limma=source("http://bioconductor.org/biocLite.R");biocLite("limma");biocLite(c("GenomicFeatures", "AnnotationDbi"))
+
+survcomp=source("http://bioconductor.org/biocLite.R");biocLite("survcomp")
+
+hgu133afrmavecs=source("http://bioconductor.org/biocLite.R");biocLite("hgu133afrmavecs")
+
+hgu133acdf=source("http://bioconductor.org/biocLite.R");biocLite("hgu133acdf")
+
+Rserve=install.packages("Rserve")
+
+R6=install.packages("R6")
+
+survivalROC=install.packages("survivalROC")
+
+install.packages('cgdsr')
+
+
+
