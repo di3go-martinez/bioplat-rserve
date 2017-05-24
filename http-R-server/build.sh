fb9bb474bc3652ef66845bb48bc9817a303ebd8e
Diego Ariel Martínez <diegoariel83@gmail.com>
2017-05-24 01:09:38 -0300

    Instalo la jre8 desde el repo oficial de ubuntu

--- http-R-server/build.sh
+++ http-R-server/build.sh
@@ -12,11 +12,6 @@ function check-file-exists(){
 
 check-file-exists webapps/R4JServer.war
 check-file-exists build/apache-tomcat-7.0.65.tar.gz 
-check-file-exists build/jre-7u45-linux-x64.tar.gz
 check-file-exists build/bioplatR-1.0.tar.gz
-
-[ -e apache-tomcat-7.0.65.tar.gz ] || echo "no está el tomcat" && exit 1 
-[ -e bioplatR-1.0.tar.gz ] || exit 2 
-[ -e jre-7u45-linux-x64.tar.gz ] || exit 3 
 
 docker build -t http-r-server .
